070fb107bba9bf30216901866c38c8289f511cac
skassaei <skassaei@torontomu.ca>
2024-09-29 21:07:23 -0400

    commit 2

--- python/translator.py
+++ python/translator.py
@@ -1,6 +1,6 @@
-
 import sys
 def EngToBri(str):
+    
     eng_to_bri_alph = {"a":"O.....","b":"O.O...","c":"OO....","d":"OO.O..","e":"O..O..","f":"OOO...","g":"OOOO..","h":"O.OO..","i":".OO...","j":".OOO..","k":"O...O.","l":"O.O.O.","m":"OO..O.","n":"OO.OO.","o":"O..OO.","p":"OOO.O.","q":"OOOOO.","r":"O.OOO.","s":".OO.O.","t":".OOOO.","u":"O...OO","v":"O.O.OO","w":".OOO.O","x":"OO..OO","y":"OO.OOO","z":"O..OOO"}
     eng_to_bri_num = {"1":"O.....","2":"O.O...","3":"OO....","4":"OO.O..","5":"O..O..","6":"OOO...","7":"OOOO..","8":"O.OO..","9":".OO...","0":".OOO.."}
     eng_to_bri_dec = {".":"..OO.O",",":"..O...","?":"..O.OO","!":"..OOO.",":":"..OO..",";":"..O.O.","-":"....OO","/":".O..O.","<":".OO..O",">":"O..OO.","(":"O.O..O",")":".O.OO."," ":"......"}
@@ -84,23 +84,22 @@ def briToEng(str):
 
         start= end
         end +=6
+
+
 def translator():
     if len(sys.argv) > 1:
-        inputString = sys.argv[1]  # Get the string from the command line
+        inputString = " ".join(sys.argv[1:])  # Combine all arguments into one string
         check_language = set(inputString)
         
+        # Decide whether to translate to Braille or to English
         if len(check_language) > 2 or ("O" not in check_language or "." not in check_language):
             # Translate English to Braille
-            print("got the input")
             EngToBri(inputString)
         else:
             # Translate Braille to English
-            print("got the input")
             briToEng(inputString)
     else:
         print("No input provided")
 
-
 if __name__ == "__main__":
     translator()
-
